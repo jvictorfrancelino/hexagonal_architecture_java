ad81004d922a1860f78a7e2997da7d761c6f3f8b
José Victor <jvictorfrance@outlook.com>
2024-11-16 06:17:04 -0300

    Removing database connection and dependencies

--- src/main/java/br/com/hexagonal_architecture/HexagonalArchitectureApplication.java
+++ src/main/java/br/com/hexagonal_architecture/HexagonalArchitectureApplication.java
@@ -2,8 +2,9 @@ package br.com.hexagonal_architecture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class HexagonalArchitectureApplication {
 
 	public static void main(String[] args) {
